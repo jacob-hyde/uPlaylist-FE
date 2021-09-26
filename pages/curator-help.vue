68c0d57f423e1fc9cd0860f09554af30b6e1681a
Jacob Hyde <jacob@jacobhyde.com>
2021-09-26 14:19:36 -0400

    Help Section

---
+++ pages/curator-help.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="article in helpArticles" :key="article.id">
+        <v-expansion-panel-header>{{
+          article.question
+        }}</v-expansion-panel-header>
+        <v-expansion-panel-content
+          class="flex-column"
+          style="padding: 16px 24px"
+          v-html="article.answer"
+        ></v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const {
+      data: { data: helpArticles },
+    } = await $axios.get('help?vendor=1')
+    return { helpArticles }
+  },
+}
+</script>
+
+<style>
+</style>
