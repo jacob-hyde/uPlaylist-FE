c189cb51fdff42f8f115331becc5b94f37627fb0
Jacob Hyde <jacob@jacobhyde.com>
2021-10-08 20:35:45 -0400

    disable paypal

--- components/Payment.vue
+++ components/Payment.vue
@@ -74,7 +74,7 @@ export default {
     paymentOptions: {
       type: Array,
       default() {
-        return ['stripe', 'paypal']
+        return ['stripe']
       },
     },
     subscription: {
